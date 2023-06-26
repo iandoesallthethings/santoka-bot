c020d59857541d7a2f95a139570ae3bb18e38a7f
iandoesallthethings <iandoesallthethings@gmail.com>
2023-06-26 15:24:11 +0200

    Fix prerender setting.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import readme from '../../README.md?raw'
 	import Markdown from '@magidoc/plugin-svelte-marked'
-
-	export const prerender = true
 </script>
 
 <Markdown source={readme} />
