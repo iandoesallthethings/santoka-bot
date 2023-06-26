7db69cbe731aa2b82d239935d9974e0329bdcf09
iandoesallthethings <iandoesallthethings@gmail.com>
2023-06-26 15:20:51 +0200

    Prerender home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import readme from '../../README.md?raw'
 	import Markdown from '@magidoc/plugin-svelte-marked'
+
+	export const prerender = true
 </script>
 
 <Markdown source={readme} />
