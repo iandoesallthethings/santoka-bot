a3bf2049f41f8b125c8931b2145444a27179022c
iandoesallthethings <iandoesallthethings@gmail.com>
2024-11-24 15:08:52 -0500

    Add docs.

--- src/lib/Promises.ts
+++ src/lib/Promises.ts
@@ -11,6 +11,18 @@ type Results<J extends JobsInput> = { [Name in keyof J]?: JobResult<J> }
  * Runs a set of promises in parallel and returns the results.
  * @param jobs JobsInput { [JobName]: Promise }
  * @returns Results { [JobName]: Result }
+ * @example
+ * const results = await runParallelJobs({
+ *    One: Promise.resolve('result-one'),
+ *    Two: Promise.resolve('result-two'),
+ *    Three: Promise.reject('Error: Three job failed'),
+ * })
+ * console.debug(results)
+ * // {
+ * //   One: { status: 'fulfilled', value: 'result-one' },
+ * //   Two: { status: 'fulfilled', value: 'result-two' },
+ * //   Three: { status: 'rejected', error: 'Error: Three job failed' }
+ * // }
  */
 export async function runParallelJobs<J extends JobsInput>(jobs: J): Promise<Results<J>> {
 	const names = Object.keys(jobs) as (keyof J)[]
