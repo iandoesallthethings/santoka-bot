e3199676ab23db05af9d4cc440e3e124254eab07
iandoesallthethings <iandoesallthethings@gmail.com>
2024-11-24 15:35:39 -0500

    Re add the part where it logs failed jobs individually.

--- src/lib/Promises.ts
+++ src/lib/Promises.ts
@@ -5,7 +5,7 @@ interface Result<T> {
 }
 type JobsInput = { [key: string]: Promise<any> }
 type JobResult<J extends JobsInput> = Result<Awaited<J[keyof J]>>
-type Results<J extends JobsInput> = { [Name in keyof J]?: JobResult<J> }
+type JobsResults<J extends JobsInput> = { [Name in keyof J]?: JobResult<J> }
 
 /**
  * Runs a set of promises in parallel and returns the results.
@@ -21,18 +21,24 @@ type Results<J extends JobsInput> = { [Name in keyof J]?: JobResult<J> }
  * // {
  * //   One: { status: 'fulfilled', value: 'result-one' },
  * //   Two: { status: 'fulfilled', value: 'result-two' },
- * //   Three: { status: 'rejected', error: 'Error: Three job failed' }
+ * //   Three: { status: 'rejected', reason: 'Error: Three job failed' }
  * // }
  */
-export async function runParallelJobs<J extends JobsInput>(jobs: J): Promise<Results<J>> {
+export async function runParallelJobs<J extends JobsInput>(jobs: J) {
 	const names = Object.keys(jobs) as (keyof J)[]
 	const promises = Object.values(jobs) as J[keyof J][]
 
 	const results = await Promise.allSettled(promises)
 
 	// Reshape the results into { [JobName]: Result }
-	return results.reduce<Results<J>>((acc, result, index) => {
+	return results.reduce<JobsResults<J>>((acc, result, index) => {
 		const jobName = names[index]
+
+		if (result.status === 'rejected') {
+			const jobName = names[index].toString()
+			console.error(`[${jobName} failed]`, result.reason)
+		}
+
 		acc[jobName] = result
 		return acc
 	}, {})
